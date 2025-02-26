0310b67f5e739e2a68a34141712f8c5ff4cf5147
Anton <2161568+INF1NUM@users.noreply.github.com>
2025-02-26 17:17:51 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,13 @@
-# Stop Update service
 echo '[GeoIP block]: Stop Update service'
-sudo systemctl disable geoip-update
+sudo systemctl disable geoip-update.timer --now
+sudo systemctl disable geoip-boot.service --now
 
-# Remove files
 echo '[GeoIP block]: Remove files'
-sudo rm /usr/local/bin/ipset-apply.sh
-sudo rm /usr/local/bin/geoip-update.sh
 sudo rm /etc/systemd/system/geoip-update.service
 sudo rm /etc/systemd/system/geoip-update.timer
-sudo rm -rf /etc/geoip_block/
-sudo rm -rf /tmp/geoip_block/
-
+sudo rm /etc/systemd/system/geoip-boot.service
+sudo rm /usr/local/bin/geoip_block
+sudo rm -rf /opt/geoip_block/
 echo '[GeoIP block]: Reload daemons'
 sudo systemctl daemon-reload
 
