24f5d874786914a2b1cc4fe1ecb4bd76084b7914
Anton <2161568+INF1NUM@users.noreply.github.com>
2025-02-26 19:38:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo mkdir -p /opt/geoip_block/bin
 sudo mkdir -p /opt/geoip_block/data
 echo '[GeoIP block]: Copy files'
 sudo cp ipset-apply.sh /opt/geoip_block/bin/ipset-apply.sh
-sudo cp geoip-update.sh /opt/geoip_block/bin/geoip-update.sh
+sudo cp geoip-block.sh /opt/geoip_block/bin/geoip-block.sh
 sudo cp url.list /opt/geoip_block/etc/url.list
 sudo cp geoip_block.conf /opt/geoip_block/etc/geoip_block.conf
 sudo cp iptables.rule /opt/geoip_block/etc/iptables.rule
@@ -22,7 +22,7 @@ sudo cp geoip-update.service /etc/systemd/system/geoip-update.service
 sudo cp geoip-update.timer /etc/systemd/system/geoip-update.timer
 sudo cp geoip-boot.service /etc/systemd/system/geoip-boot.service
 sudo chmod +x /opt/geoip_block/bin/ipset-apply.sh
-sudo chmod +x /opt/geoip_block/bin/geoip-update.sh
+sudo chmod +x /opt/geoip_block/bin/geoip-block.sh
 ln -s /opt/geoip_block/bin/geoip-block.sh /usr/local/bin/geoip_block
 
 echo '[GeoIP block]: Start Update service'
