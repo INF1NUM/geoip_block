60dfc9db8793d8f1b5ad62c3ac138c3a52f596a0
Anton <2161568+INF1NUM@users.noreply.github.com>
2025-03-18 15:10:04 +0300

    Update install.sh
    link services

--- install.sh
+++ install.sh
@@ -19,16 +19,18 @@ sudo cp geoip-block.sh /opt/geoip_block/bin/geoip-block.sh
 sudo cp url.list /opt/geoip_block/etc/url.list
 sudo cp geoip_block.conf /opt/geoip_block/etc/geoip_block.conf
 sudo cp iptables.rule /opt/geoip_block/etc/iptables.rule
-sudo cp geoip-update.service /etc/systemd/system/geoip-update.service
-sudo cp geoip-update.timer /etc/systemd/system/geoip-update.timer
-sudo cp geoip-boot.service /etc/systemd/system/geoip-boot.service
+sudo cp geoip-update.service /opt/geoip_block/etc/geoip-update.service
+sudo cp geoip-update.timer /opt/geoip_block/etc/geoip-update.timer
+sudo cp geoip-boot.service /opt/geoip_block/etc/geoip-boot.service
 sudo chmod +x /opt/geoip_block/bin/ipset-apply.sh
 sudo chmod +x /opt/geoip_block/bin/geoip-block.sh
 sudo ln -s /opt/geoip_block/bin/geoip-block.sh /usr/local/bin/geoip_block
 
 echo '[GeoIP block]: Start Update service'
-sudo systemctl daemon-reload
-sudo systemctl enable geoip-update.timer --now
+sudo systemctl link /opt/geoip_block/etc/geoip-update.service
+sudo systemctl link /opt/geoip_block/etc/geoip-update.timer
+sudo systemctl link /opt/geoip_block/etc/geoip-boot.service
 sudo systemctl enable geoip-boot.service
+sudo systemctl enable geoip-update.timer --now
 
 echo '[GeoIP block]: Install complete'
