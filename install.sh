c41853cdba01189b47a8b62ac80fb2a00559fcfc
Anton <agergiev@ya.ru>
2025-03-20 14:26:27 +0300

    Optimization

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+APPDIR='/opt/geoip_block'
+
 echo '[GeoIP block]: Installing dependencies...'
 sudo apt install ipset iprange -y
 
@@ -9,26 +11,29 @@ rm main.tar.gz
 cd geoip_block-main/
 
 echo '[GeoIP block]: Сreating a directory structure...'
-sudo mkdir -p /opt/geoip_block/{etc,bin,data}
+sudo mkdir -p ${APPDIR}/{etc,bin,data}
 
 echo '[GeoIP block]: Сopying program files...'
-sudo cp ipset-apply.sh /opt/geoip_block/bin/ipset-apply.sh
-sudo cp geoip-block.sh /opt/geoip_block/bin/geoip-block.sh
-sudo cp url.list /opt/geoip_block/etc/url.list
-sudo cp geoip_block.conf /opt/geoip_block/etc/geoip_block.conf
-sudo cp iptables.rule /opt/geoip_block/etc/iptables.rule
-sudo cp geoip-update.service /opt/geoip_block/etc/geoip-update.service
-sudo cp geoip-update.timer /opt/geoip_block/etc/geoip-update.timer
-sudo cp geoip-boot.service /opt/geoip_block/etc/geoip-boot.service
-sudo chmod +x /opt/geoip_block/bin/ipset-apply.sh
-sudo chmod +x /opt/geoip_block/bin/geoip-block.sh
-sudo ln -s /opt/geoip_block/bin/geoip-block.sh /usr/local/bin/geoip_block
+sudo cp ipset-apply.sh "${APPDIR}/bin/ipset-apply.sh"
+sudo cp geoip-block.sh "${APPDIR}/bin/geoip-block.sh"
+sudo cp url.list "${APPDIR}/etc/url.list"
+sudo cp geoip_block.conf "${APPDIR}/etc/geoip_block.conf"
+sudo cp iptables.rule "${APPDIR}/etc/iptables.rule"
+sudo cp geoip-update.service "${APPDIR}/etc/geoip-update.service"
+sudo cp geoip-update.timer "${APPDIR}/etc/geoip-update.timer"
+sudo cp geoip-boot.service "${APPDIR}/etc/geoip-boot.service"
+sudo chmod +x "${APPDIR}/bin/ipset-apply.sh"
+sudo chmod +x "${APPDIR}/bin/geoip-block.sh"
+sudo ln -s "${APPDIR}/bin/geoip-block.sh" "/usr/local/bin/geoip_block"
 
-echo '[GeoIP block]: Setting up Update service'
-sudo systemctl link /opt/geoip_block/etc/geoip-update.service
-sudo systemctl link /opt/geoip_block/etc/geoip-update.timer
-sudo systemctl link /opt/geoip_block/etc/geoip-boot.service
+echo '[GeoIP block]: Setting up Update service...'
+sudo systemctl link ${APPDIR}/etc/geoip-update.service
+sudo systemctl link ${APPDIR}/etc/geoip-update.timer
+sudo systemctl link ${APPDIR}/etc/geoip-boot.service
 sudo systemctl enable geoip-boot.service
 sudo systemctl enable geoip-update.timer --now
+
+echo '[GeoIP block]: Cleaning...'
+rm -rf ../geoip_block-main
 
 echo '[GeoIP block]: Install complete'
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo '[GeoIP block]: Stop services...'
+echo '[GeoIP block]: Uninstall services...'
 sudo systemctl disable geoip-update.timer geoip-update.service geoip-boot.service
 sudo systemctl reset-failed
 sudo systemctl daemon-reload
 
 echo '[GeoIP block]: Remove files'
-sudo rm /etc/systemd/system/geoip-update.service
-sudo rm /etc/systemd/system/geoip-update.timer
-sudo rm /etc/systemd/system/geoip-boot.service
-sudo rm /usr/local/bin/geoip_block
-sudo rm -rf /opt/geoip_block/
+sudo rm /etc/systemd/system/geoip-update.service 2> /dev/null
+sudo rm /etc/systemd/system/geoip-update.timer 2> /dev/null
+sudo rm /etc/systemd/system/geoip-boot.service 2> /dev/null
+sudo rm /usr/local/bin/geoip_block  2> /dev/null
+sudo rm -rf /opt/geoip_block/  2> /dev/null
 
 echo '[GeoIP block]: Uninstall complete'
