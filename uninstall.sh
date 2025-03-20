cc2aedddeb69637344ddf1e3c9aefb88c2192330
Anton <agergiev@ya.ru>
2025-03-20 14:33:37 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo '[GeoIP block]: Uninstall services...'
+echo '[GeoIP block]: Uninstalling services...'
 sudo systemctl disable geoip-update.timer geoip-update.service geoip-boot.service 2> /dev/null
 sudo systemctl reset-failed
 sudo systemctl daemon-reload
 
-echo '[GeoIP block]: Remove files'
+echo '[GeoIP block]: Removing programm files...'
 sudo rm /etc/systemd/system/geoip-update.service 2> /dev/null
 sudo rm /etc/systemd/system/geoip-update.timer 2> /dev/null
 sudo rm /etc/systemd/system/geoip-boot.service 2> /dev/null
