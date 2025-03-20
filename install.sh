be8af08086c6f996e3c714094a16c5d36219f543
Anton <agergiev@ya.ru>
2025-03-20 14:42:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,22 +14,22 @@ echo '[GeoIP block]: Сreating a directory structure...'
 sudo mkdir -p ${APPDIR}/{etc,bin,data}
 
 echo '[GeoIP block]: Сopying program files...'
-sudo cp ipset-apply.sh "${APPDIR}/bin/ipset-apply.sh"
-sudo cp geoip-block.sh "${APPDIR}/bin/geoip-block.sh"
-sudo cp url.list "${APPDIR}/etc/url.list"
-sudo cp geoip_block.conf "${APPDIR}/etc/geoip_block.conf"
-sudo cp iptables.rule "${APPDIR}/etc/iptables.rule"
-sudo cp geoip-update.service "${APPDIR}/etc/geoip-update.service"
-sudo cp geoip-update.timer "${APPDIR}/etc/geoip-update.timer"
-sudo cp geoip-boot.service "${APPDIR}/etc/geoip-boot.service"
-sudo chmod +x "${APPDIR}/bin/ipset-apply.sh"
-sudo chmod +x "${APPDIR}/bin/geoip-block.sh"
-sudo ln -s "${APPDIR}/bin/geoip-block.sh" "/usr/local/bin/geoip_block"
+sudo cp ipset-apply.sh ${APPDIR}/bin/ipset-apply.sh
+sudo cp geoip-block.sh ${APPDIR}/bin/geoip-block.sh
+sudo cp url.list ${APPDIR}/etc/url.list
+sudo cp geoip_block.conf ${APPDIR}/etc/geoip_block.conf
+sudo cp iptables.rule ${APPDIR}/etc/iptables.rule
+sudo cp geoip-update.service ${APPDIR}/etc/geoip-update.service
+sudo cp geoip-update.timer ${APPDIR}/etc/geoip-update.timer
+sudo cp geoip-boot.service ${APPDIR}/etc/geoip-boot.service
+sudo chmod +x ${APPDIR}/bin/ipset-apply.sh
+sudo chmod +x ${APPDIR}/bin/geoip-block.sh
+sudo ln -s ${APPDIR}/bin/geoip-block.sh" "/usr/local/bin/geoip_block
 
 echo '[GeoIP block]: Setting up Update service...'
-sudo systemctl link "${APPDIR}/etc/geoip-update.service"
-sudo systemctl link "${APPDIR}/etc/geoip-update.timer"
-sudo systemctl link "${APPDIR}/etc/geoip-boot.service"
+sudo systemctl link ${APPDIR}/etc/geoip-update.service
+sudo systemctl link ${APPDIR}/etc/geoip-update.timer
+sudo systemctl link ${APPDIR}/etc/geoip-boot.service
 sudo systemctl enable geoip-boot.service
 sudo systemctl enable geoip-update.timer --now
 
